89ffb44ae05faf1eb54be6c90bf9ab56fec16b34
Hazeen Dodhiya <hazeenwork@gmail.com>
2026-05-03 18:48:48 +0500

    added dark color

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className="bg-gray-950 text-white">
 
         {/* NAVBAR - shows on every page */}
